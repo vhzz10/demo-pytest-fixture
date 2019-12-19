3c19da35fd4b637aa2c4d75d4f93133eed8dca7a
namgivu <namgivu@gmail.com>
2019-12-19 16:35:38 +0700

    use func.__name__

--- tests/test_demo_use_fx.py
+++ tests/test_demo_use_fx.py
@@ -1,25 +1,29 @@
 import pytest
 from falcon import testing
 
+from conftest import demo_no_yield, demo_share_fx
+
+
 @pytest.fixture
 def demo_local_fx():
     print('Start LOCAL fx')
     yield
     print('Done LOCAL fx')
 
+
 class Test(testing.TestCase):
 
-    @pytest.mark.usefixtures('demo_no_yield')
+    @pytest.mark.usefixtures(demo_no_yield.__name__)
     def test1(self):
         print('Run TC 1')
         pass
 
-    @pytest.mark.usefixtures('demo_local_fx')
+    @pytest.mark.usefixtures(demo_local_fx.__name__)
     def test2(self):
         print('Run TC 2')
         pass
 
-    @pytest.mark.usefixtures('demo_share_fx')
+    @pytest.mark.usefixtures(demo_share_fx.__name__)
     def test3(self):
         print('Run TC 3')
         pass

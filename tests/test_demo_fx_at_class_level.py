0ae2c4f1c4aad6a81dd8beaf7e76415e6c164b9a
namgivu <nam.gi.vu@gigacover.com>
2020-02-07 16:16:16 +0700

    separated file for class-level

---
+++ tests/test_demo_fx_at_class_level.py
@@ -0,0 +1,25 @@
+import pytest
+from falcon import testing
+
+from conftest import fx_1, fx_2, fx_3
+from tests.this_method import this_method
+
+
+@pytest.mark.usefixtures(fx_1.__name__)  # class-level fixture will run after method-level fixture(s)
+class Test_usefixtures_at_class_level(testing.TestCase):
+
+    def test1(self):
+        print(f'Run {this_method()}')
+
+
+    @pytest.mark.usefixtures(fx_2.__name__)
+    def test2(self):
+        print(f'Run {this_method()}')
+
+
+    @pytest.mark.usefixtures(
+        fx_2.__name__,
+        fx_3.__name__,
+    )
+    def test3(self):
+        print(f'Run {this_method()}')
--- tests/test_demo_use_fx.py
+++ tests/test_demo_use_fx.py
@@ -1,7 +1,7 @@
 import pytest
 from falcon import testing
 
-from conftest import demo_no_yield, demo_share_fx, demo_external_fx, fx_1, fx_2, fx_3
+from conftest import demo_no_yield, demo_share_fx, demo_external_fx
 from tests.this_method import this_method
 
 @pytest.fixture
@@ -38,22 +38,4 @@ class Test1(testing.TestCase):
 
     @pytest.mark.usefixtures(demo_external_fx.__name__)
     def test5(self):
-        print(f'Run {this_method()}')
-
-
-@pytest.mark.usefixtures(fx_1.__name__)
-class Test2_usefixtures_at_class_level(testing.TestCase):
-
-    def test1(self):
-        print(f'Run {this_method()}')
-
-    @pytest.mark.usefixtures(fx_2.__name__)
-    def test2(self):
-        print(f'Run {this_method()}')
-
-    @pytest.mark.usefixtures(
-        fx_2.__name__,
-        fx_3.__name__,
-    )
-    def test3(self):
         print(f'Run {this_method()}')
